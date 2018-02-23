e372f93330e92834d6f10db81fd7eedc3d2a2a65
M. Folkers <hello@twobrain.io>
2018-02-23 18:01:39 +0100

    Making javascripts work

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -4,123 +4,34 @@
  * Loading dependencies
  */
 
-import zenscroll from 'zenscroll';
 import Barba from 'barba.js';
+import zenscroll from 'zenscroll';
 import { tns } from 'tiny-slider/src/tiny-slider.module';
 
 
-
 /*
- * Declaring custom functions
+ * Initializing all the things
  */
 
-// 1. Simple JS feature detection
-
-function featureDetection() {
+window.onload = function() {
   let className = '';
   let html = '';
   html = document.documentElement;
   className = html.className.replace('no-js', 'js');
   html.className = className;
-}
-
-
-// 2. tinySlider
-
-function tinySlider() {
-  // const sliderElement = document.getElementById('slider');
-  // const sliderControls = document.getElementById('slider-controls');
-
-  let slider = tns({
-    container: '.slider',
-    controlsContainer: '.slider-controls',
-    // container: sliderElement,
-    // controlsContainer: sliderControls,
-    gutter: 10,
-
-    slideBy: 1,
-    speed: 1250,
-
-    autoplay: true,
-    autoplayTimeout: 3500,
-    autoplayHoverPause: true,
-
-    responsive: {
-      '0': 1,
-      '600': 2,
-      '900': 3,
-      // '1200': 4
-    },
-  });
-}
-
-
-// 4. OSM - Map
-
-// var Routing = require('leaflet-routing-machine');
-// var Geocoder = require('leaflet-control-geocoder');
-
-function mapInit() {
-  const layer = new L.StamenTileLayer('watercolor');
-
-  const seiring_map = L.map('map_canvas',{
-    scrollWheelZoom: false
-  }).setView([47.98905, 7.85686], 100).addLayer(layer);
-  // }).setView([47.98905, 7.85686], 19);
-
-  L.marker([47.98905, 7.85686]).addTo(seiring_map)
-    .bindPopup(
-      '<b>Seiring & Seiring Rechtsanwälte</b><br>' +
-      'Dreikönigsstraße 12<br>' +
-      '79102 Freiburg im Breisgau'
-    )
-    .openPopup();
-
-    // ODER
-
-    // L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   maxZoom: 19,
-    //   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    // }).addTo(seiring_map);
-
-
-    // Routing capability
-
-    // L.Routing.control({
-    //     waypoints: [
-    //         L.latLng(),
-    //         L.latLng(47.98905, 7.85686)
-    //     ],
-    //     routeWhileDragging: true,
-    //     geocoder: L.Control.Geocoder.nominatim()
-    // }).addTo(seiring_map);
-
-
-    // map.remove();
-    // var node  = document.getElementById('map_canvas');
-    // node.parentNode.removeChild(node);
-
-}
-
-
-
-/*
- * Initializing functions
- */
-
-
-window.onload = function() {
-  featureDetection();
-  tinySlider();
 };
 
 
-// Barba-specific
+/*
+ * Setting up barba.js
+ */
 
-// Define barba properties
-Barba.transitionLength = 1000;
+// Variables
+Barba.Pjax.Dom.wrapperId = 'js-content';
+Barba.Pjax.Dom.containerClass = 'wrapper';
+Barba.transitionLength = 500;
 
-// Define transition
+// Transitions
 var Transition = Barba.BaseTransition.extend({
   start: function() {
     this.newContainerLoading.then(this.runTransition.bind(this));
@@ -208,25 +119,31 @@ var Transition = Barba.BaseTransition.extend({
   }
 });
 
-// Add transition to barba
-Barba.Pjax.getTransition = function() {
-  return Transition;
-};
-
-
-//
-// Barba.js defintions
-
-// Variables
-Barba.Pjax.Dom.wrapperId = 'js-content';
-Barba.Pjax.Dom.containerClass = 'wrapper';
-
 // Views
-const MapView = Barba.BaseView.extend({
-  namespace: 'kontakt',
+const HomeView = Barba.BaseView.extend({
+  namespace: 'home',
   onEnter: function() {
     // The new Container is ready and attached to the DOM.
-    mapInit();
+    let slider = tns({
+      container: '.slider',
+      controlsContainer: '.slider-controls',
+      gutter: 10,
+      nav: false,
+
+      slideBy: 1,
+      speed: 750,
+
+      autoplay: true,
+      autoplayTimeout: 3500,
+      autoplayHoverPause: true,
+
+      responsive: {
+        '0': 1,
+        '600': 2,
+        '900': 3,
+        // '1200': 4
+      },
+    });
   },
   onEnterCompleted: function() {
     // The Transition has just finished.
@@ -239,24 +156,56 @@ const MapView = Barba.BaseView.extend({
   }
 });
 
-// Don't forget to init the view!
+HomeView.init();
+
+const MapView = Barba.BaseView.extend({
+  namespace: 'kontakt',
+  onEnter: function() {
+    // The new Container is ready and attached to the DOM.
+    const layer = new L.StamenTileLayer('watercolor');
+    const seiring_map = L.map('map_canvas',{
+      scrollWheelZoom: false
+    }).setView([47.98905, 7.85686], 100).addLayer(layer);
+    // }).setView([47.98905, 7.85686], 19);
+
+    L.marker([47.98905, 7.85686]).addTo(seiring_map)
+      .bindPopup(
+        '<b>Seiring & Seiring Rechtsanwälte</b><br>' +
+        'Dreikönigsstraße 12<br>' +
+        '79102 Freiburg im Breisgau'
+      )
+      .openPopup();
+  },
+  onEnterCompleted: function() {
+    // The Transition has just finished.
+  },
+  onLeave: function() {
+    // A new Transition toward a new page has just started.
+  },
+  onLeaveCompleted: function() {
+    // The Container has just been removed from the DOM.
+  }
+});
+
 MapView.init();
 
+Barba.Pjax.getTransition = function() { return Transition; };
 Barba.Pjax.start();
 Barba.Prefetch.init();
 
 Barba.Dispatcher.on('linkClicked', function(element) {
-  const activeNavElements = document.getElementsByClassName('is-active');
-  let i;
 
-  for (i = 0; i < activeNavElements.length; i += 1) {
+  const header = document.getElementById('js-header');
+
+  const activeNavElements = header.getElementsByClassName('is-active');
+
+  for (let i = 0; i < activeNavElements.length; i += 1) {
     activeNavElements[i].classList.remove('is-active');
   }
   // var mobileMenu = document.querySelector('.navigation-links');
   // mobileMenu.classList.add('hidden');
 
   const elementLink = element.getAttribute('href');
-  const header = document.getElementById('js-header');
   const navLinkToAddActive = header.querySelector(`a[href="${elementLink}"]`);
 
   try {
@@ -272,14 +221,12 @@ Barba.Dispatcher.on('linkClicked', function(element) {
 // });
 
 Barba.Dispatcher.on('newPageReady', function() {
-  tinySlider();
+  // zenscroll.toY(0);
+  // window.scrollTo(0, 0);
+
 });
 
 Barba.Dispatcher.on('transitionCompleted', function() {
   window.scrollTo(0, 0);
+  // zenscroll.toY(0);
 });
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//
-// });
